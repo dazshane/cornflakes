ce45bba26bfe9d5842a0a70474cfcff224a99a8d
Marek Stosic <xstosic@fi.muni.cz>
2021-05-13 22:12:15 +0200

    WIP Backup #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Stack, TextField, Checkbox, DefaultButton, DatePicker, mergeStyles, IStackStyles, IStackTokens, DefaultPalette } from '@fluentui/react';
+import { Stack, TextField, Checkbox, DefaultButton, DatePicker, mergeStyles, IStackStyles, IStackTokens, DefaultPalette, MessageBar, MessageBarType, Link, Check } from '@fluentui/react';
 import React, { useState } from 'react';
 import { Dropdown, DropdownMenuItemType, IDropdownStyles, IDropdownOption } from '@fluentui/react/lib/Dropdown';
 
 const dropdownStyles: Partial<IDropdownStyles> = {
-  dropdown: { width: 400 },
+  dropdown: { width: 500 },
 };
 
 const stackStyles: IStackStyles = {
@@ -12,7 +12,7 @@ const stackStyles: IStackStyles = {
 };
 
 const horizontalGapStackTokens: IStackTokens = {
-  childrenGap: 100,
+  childrenGap: 200,
 };
 
 const options: IDropdownOption[] = [
@@ -32,17 +32,23 @@ const options: IDropdownOption[] = [
   { key: 'Moravskoslezský kraj', text: 'Moravskoslezský kraj' },
 ];
 
-const rootClass = mergeStyles({ maxWidth: 400, selectors: { '> *': { marginBottom: 15 } } });
+const rootClass = mergeStyles({ maxWidth: 500, selectors: { '> *': { marginBottom: 15 } } });
 
-function App() {
+function App(): JSX.Element {
 
   const [jmeno, setJmeno] = useState("");
   const [prijmeni, setPrijmeni] = useState("");
   const [email, setEmail] = useState("");
-  const [date, setDate] = useState(new Date('May 13, 2021 17:05:00'));
-  const [kraj, setKraj] = useState<IDropdownOption>();
+  const [date, setDate] = useState<Date | null>(null);
+  const [kraj, setKraj] = useState<IDropdownOption | null>(null);
   const [react, setReact] = useState(false);
   const [multi, setMulti] = useState("");
+
+  const [emailerror, setEmailError] = useState(false);
+  const [confirm, setConfirm] = useState(false);
+  const [jsonerror, setJsonError] = useState(false);
+
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   return (
     <Stack className={rootClass}>
@@ -52,38 +58,94 @@ function App() {
       </Stack>
 
       <Stack horizontal styles={stackStyles} tokens={horizontalGapStackTokens}>
-        <TextField label="E-mail" value={email} onChange={(event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => setEmail(String(newValue))}></TextField>
-        <DatePicker label="Datum narození" value={date} onSelectDate={(date: Date | null | undefined) => date && setDate(date)}/>
+        <TextField label="E-mail" value={email} onChange={(event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {setEmail(String(newValue))}}></TextField>
+        <DatePicker label="Datum narození" value={date===null?undefined:date} onSelectDate={(date: Date | null | undefined) => date && setDate(date)}/>
       </Stack>
 
       <Dropdown
         label="Kraj"
         options={options}
         styles={dropdownStyles}
-        selectedKey={kraj?.key as string}
+        selectedKey={kraj===null?null:kraj.key as string}
         onChange={(event: React.FormEvent<HTMLDivElement>, option?: IDropdownOption, index?: number) => option && setKraj(option)}
       />
 
       <Checkbox label="REACT" checked={react} onChange={(ev?: React.FormEvent<HTMLElement | HTMLInputElement>, checked?: boolean) => setReact(Boolean(checked))} />
 
       <Stack horizontal styles={stackStyles} tokens={horizontalGapStackTokens}>
-        <DefaultButton text="Uložit" onClick={() => setMulti(JSON.stringify({jmeno, prijmeni, email, date, kraj, react}))} />
+        <DefaultButton text="Uložit" onClick={() => check()} />
         <DefaultButton text="Načíst" onClick={multi => setAll()} />
       </Stack>
 
       <TextField multiline rows={7} value={multi} onChange={(event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => setMulti(String(newValue))}/>
+
+      {emailerror? <MessageBar
+        messageBarType={MessageBarType.error}
+        isMultiline={false}
+        onDismiss={() => setEmailError(false)}
+        dismissButtonAriaLabel="Close"
+      >
+        Error: Špatný formát E-mailu (správný formát: jméno@domain)
+      </MessageBar>:null}
+
+      {confirm? <MessageBar
+        messageBarType={MessageBarType.success}
+        isMultiline={false}
+        onDismiss={() => setConfirm(false)}
+        dismissButtonAriaLabel="Close"
+      >
+        Uložení proběhlo úspěšně
+      </MessageBar>:null}
+
+      {jsonerror? <MessageBar
+        messageBarType={MessageBarType.error}
+        isMultiline={false}
+        onDismiss={() => setJsonError(false)}
+        dismissButtonAriaLabel="Close"
+      >
+        Error: Invalid Json
+      </MessageBar>:null}
+
     </Stack>
   );
 
   function setAll() {
-    var result = JSON.parse(String(multi));
-    setJmeno(result.jmeno);
-    setPrijmeni(result.prijmeni);
-    setEmail(result.email);
-    setDate(new Date(result.date));
-    setKraj(result.kraj);
-    setReact(result.react);
+    if (multi !== '')
+    {
+      var result = JSON.parse(String(multi));
+      setMulti("");
+
+      if (result.jmeno === undefined || result.prijmeni === undefined || result.email === undefined || result.date === undefined || result.kraj === undefined || result.react === undefined)
+      {
+        setJsonError(true);
+      }
+      else
+      {
+        setJmeno(result.jmeno);
+        setPrijmeni(result.prijmeni);
+        setEmail(result.email);
+        setDate(new Date(result.date) as Date);
+        setKraj(result.kraj);
+        setReact(result.react);
+      }
+    }
   };
+
+  function check() {
+    if (re.test(email)) {
+      setMulti(JSON.stringify({jmeno, prijmeni, email, date, kraj, react}));
+      setJmeno("");
+      setPrijmeni("");
+      setEmail("");
+      setDate(null);
+      setKraj(null);
+      setReact(false);
+      setConfirm(true);
+    }
+    else {
+      setEmailError(true);
+    }
+  }
 }
 
 export default App;
